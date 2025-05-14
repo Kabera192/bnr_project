5e8650451b56722a38db9bfa7e62f862696f57cf
Kabera192 <ckabera6@gmail.com>
2025-05-14 10:25:08 +0200

    initial project structure

---
+++ src/test/java/rw/bnr/BnrApplicationTests.java
@@ -0,0 +1,13 @@
+package rw.bnr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BnrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
